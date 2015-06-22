1bd4a95ca14900d8ccbd25cea901454d9086757c
Christopher Robison <cdr@cdr2.com>
2015-06-21 23:06:36 -0700

    Initial Commit

---
+++ Beaker2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beaker2
+//
+//  Created by Christopher Robison on 6/21/15.
+//  Copyright (c) 2015 Christopher Robison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
